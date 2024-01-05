e3674f223380e1f1dc3afbe085e86a5c2d8c70e5
jimmy-to-goldenowl <jimmy.to.goldenowl@gmail.com>
2024-01-05 14:47:22 +0700

    Initial Commit

---
+++ swift-boilerplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-boilerplate
+//
+//  Created by Khánh Tô on 05/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
